7c65998137b1e2f5eb0bf5f0c55eccb670968072
Jared Smith <jared.smith.jrod@gmail.com>
2015-03-07 19:37:40 -0800

    another update

--- init.el
+++ init.el
@@ -15,12 +15,9 @@
 (color-theme-approximate-on)
 ;; Why no rainbow-delimiter-global-mode ?
 (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'org-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'org-mode-hook  #'rainbow-delimiters-mode)
 ;; Paredit sucks
 (autopair-global-mode t)
-;; Rust stuff
-(require 'rust-mode)
-(require 'racer)
 ; Org-mode
 ;; LaTeX export options
 
@@ -30,28 +27,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(buffers-menu-show-directories t)
  '(custom-enabled-themes (quote (Glacial-Dark)))
- '(custom-safe-themes
-   (quote
-    ("4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" "058eeda70c6a8e702da4c7037c582f727887f5ab49a401247eac40aece97060d" "7dc68389ff027b898cd4ed7b2c42018e4d4d8e34a6036c1c2ba94c18f6bfcfc3" default)))
+ '(custom-safe-themes (quote ("43a1307e24e1622534de4317cfa912e22acf92da55e44646563bdea0059dff49" default)))
  '(custom-theme-directory "~/.emacs.d/themes")
  '(custom-theme-load-path (quote (custom-theme-directory t)))
  '(display-battery-mode t)
+ '(fringe-mode nil nil (fringe))
  '(global-nlinum-mode t)
  '(inhibit-startup-screen t)
  '(max-mini-window-height 2)
  '(minibuffer-auto-raise nil)
- '(mode-line-format
-   (quote
-    ("%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position
-     (vc-mode vc-mode)
-     "  " mode-line-modes mode-line-misc-info mode-line-end-spaces)))
+ '(mode-line-format (quote ("%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position (vc-mode vc-mode) "  " mode-line-modes mode-line-misc-info mode-line-end-spaces)))
  '(omnisharp-host "http://localhost:5004/")
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(org-startup-truncated nil)
+ '(overflow-newline-into-fringe nil)
  '(scroll-bar-mode nil)
  '(size-indication-mode t)
  '(tool-bar-mode nil))
@@ -65,8 +57,8 @@
 
 ;; Transparency
 ;;; ( ... 'alpha '(<active> [inactive]))
-(set-frame-parameter (selected-frame) 'alpha '(90 80))
-(add-to-list 'default-frame-alist '(alpha 90 80))
+(set-frame-parameter (selected-frame) 'alpha '(95 90))
+(add-to-list 'default-frame-alist '(alpha 95 90))
 ;; C-c t: Keymap switch for on/off
 (eval-when-compile (require 'cl))
 (defun toggle-transparency ()
@@ -75,7 +67,7 @@
        (cadr (frame-parameter nil 'alpha))
        100)
       (set-frame-parameter nil 'alpha '(100 100))
-    (set-frame-parameter nil 'alpha '(90 80))))
+    (set-frame-parameter nil 'alpha '(95 90))))
 (global-set-key (kbd "C-c t") 'toggle-transparency)
 
 ;; --------------------------------------------------
@@ -85,6 +77,7 @@
 ;; --------------------------------------------------
 (if (eq system-type 'darwin)
     (progn
+      (message "success osx")
 
       ;; fixes 'too long for unix domain', could be a problem on other platforms
       ;; confirmed problem on osx
@@ -99,6 +92,10 @@
 		      "/usr/local/bin" ":" "/bin" ":" (getenv "PATH")))
       
       (osx-clipboard-mode +1)
+
+      ;; Rust stuff
+      (require 'rust-mode)
+      (require 'racer)
       
       ;; enable mouse reporting for terminal emulators
       (unless window-system
@@ -120,9 +117,14 @@
 ;; Linux Specific
 ;
 ;; --------------------------------------------------
-(if (eq system-type 'linux)
+(if (eq system-type 'gnu/linux)
     (progn
-      (message "success linux")
+      (message "success gnu/linux")
+
+      (set-face-attribute 'default nil :height 100)
+
+      (setq visible-bell 0)
+
       ))
 ;; End Linux
 
